47689e3cd60e3281d4ffa1c2e71d0fbdc9be0278
chrislush <133811141+chrislush@users.noreply.github.com>
2025-02-04 16:46:16 +0000

    adding button jump feature

--- workshop/mischievous/script.js
+++ workshop/mischievous/script.js
@@ -1,10 +1,28 @@
-// Toggle hidden information
-const toggleButton = document.getElementById('btn-toggle1');
+// Enable button jumping
+const jumpingButton = document.getElementById('btn-toggle1');
 const hiddenInfo = document.querySelector('.hidden-info');
+const sectionOneHeading = document.querySelector("#section1 > h2");
+const sectionOneParagraph = document.querySelector("#section1 > p");
+sectionOneHeading.innerText = "Press the button below for a fun surprise!";
+sectionOneHeading.style.fontSize = "28px";
+sectionOneParagraph.innerText = "";
 
-toggleButton.addEventListener('click', () => {
-    hiddenInfo.classList.toggle('hidden-info');
-});
+jumpingButton.innerText = 'PRESS ME';
+jumpingButton.style.borderRadius = "100%";
+jumpingButton.style.color = "beige";
+jumpingButton.style.fontSize = "20px";
+jumpingButton.style.height = "100px";
+jumpingButton.style.width = "100px";
+jumpingButton.style.backgroundImage = "radial-gradient(#ad1818 55%, #000000)";
+jumpingButton.style.display = "block";
+jumpingButton.style.margin = "auto";
+document.getElementById("section1").style.textAlign = "center";
+
+jumpingButton.onmouseenter = () => {
+    jumpingButton.style.position = "absolute";
+    jumpingButton.style.top = `${Math.floor(Math.random() * 1000)}px`;
+    jumpingButton.style.left = `${Math.floor(Math.random() * 1000)}px`;
+};
 
 // Change background color of the box
 const colorButton = document.getElementById('btn-change-color');
